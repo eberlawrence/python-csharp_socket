71ede133ced97711df86e645e6c0ae9550901463
Eber Lawrence <eberlawrence@hotmail.com>
2019-10-30 18:12:56 -0300

    Update.

--- python_server.py
+++ python_server.py
@@ -4,20 +4,9 @@ PORT = 9999            # Porta que o Servidor esta
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 orig = (HOST, PORT)
 udp.bind(orig)
-
-EMG = []
-
-
 while True:
     msg, cliente = udp.recvfrom(1024)
-    EMG.append(int(msg))   
-    
-    print(EMG)
-    
+    print(cliente, msg)
 udp.close()
-
-
-
-
 
 
